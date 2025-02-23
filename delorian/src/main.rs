0b27ff099948549e032304008b66e13fb591bb7b
IFFranciscoME <if.francisco.me@gmail.com>
2025-02-22 22:05:48 -0600

    improvements in calling

--- delorian/src/main.rs
+++ delorian/src/main.rs
@@ -2,53 +2,34 @@
 //!
 
 use anyhow::Result;
-use delorian_data::{clients, files, processing};
+use delorian_data::{clients, files};
+use delorian_analytics::{AnalyticsTable, analyze_tx};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let v_signatures = files::read_cases_json("./data/exp_1_cases.json");
 
+    let tkn_env = "0f34064d-5762-4b3f-af49-319920aa09b9";
+    let url_env = "https://mainnet.helius-rpc.com/?api-key=";
+
+    println!("url_env: {:?}", url_env);
+
+    let h_client = clients::HeliusRpcBuilder::new()
+        .url(url_env.to_string())
+        .tkn(tkn_env.to_string())
+        .build()
+        .expect("Failed to build HeliusRpc");
+    
     for i_signature in v_signatures.unwrap() {
-        let tx_response = clients::get_transaction(&i_signature).await?;
-
-        let compute_units = tx_response
-            .result
-            .as_ref()
-            .unwrap()
-            .meta
-            .compute_units_consumed;
-
-        let log_messages = tx_response
-            .result
-            .as_ref()
-            .unwrap()
-            .meta
-            .log_messages
-            .clone();
-
-        let grep_msg = processing::grep_messages(&log_messages.as_ref().unwrap());
-
-        let fees = tx_response.result.as_ref().unwrap().meta.fee;
+        
+        let tx_response = h_client.get_tx(&i_signature).await?;        
+        let analytics_table: AnalyticsTable = analyze_tx(tx_response.clone());
 
         println!("\n -- tx_signature: {:?} -- \n", i_signature);
-        println!(
-            "Compute Units: {:?}\nFees: {:?}\ncompute_budget calls: {:?}",
-            compute_units,
-            fees,
-            grep_msg.len()
-        );
+        println!("Analytics Table: {:?}", analytics_table);
 
-        let instruction_data = tx_response
-            .result
-            .unwrap()
-            .transaction
-            .message
-            .unwrap()
-            .instructions
-            .unwrap();
-
-        println!("\ninstructions: {:?}\n", instruction_data);
     }
 
     Ok(())
 }
+
